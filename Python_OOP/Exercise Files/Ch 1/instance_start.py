b549145c2240b240f94a74e294ca01c149525a7c
Hiruna Senarath <49858479+HirunaSenarath@users.noreply.github.com>
2023-01-19 12:15:45 +0530

    python create instance exercise code

--- Python_OOP/Exercise Files/Ch 1/instance_start.py
+++ Python_OOP/Exercise Files/Ch 1/instance_start.py
@@ -5,21 +5,37 @@
 class Book:
     # the "init" function is called when the instance is
     # created and ready to be initialized
-    def __init__(self, title):
+    def __init__(self, title, author, pages, price):
         self.title = title
         # TODO: add properties
+        self.author = author
+        self.pages = pages
+        self.price = price
+        self.__secret = "This is a secret"
 
     # TODO: create instance methods
+    def getprice(self):
+        if hasattr(self, "_discount"):
+            return self.price - (self.price * self._discount)
+        else:
+            return self.price
+
+    def setdiscount(self, amount):
+        self._discount = amount
 
 
 # TODO: create some book instances
-b1 = Book("War and Peace")
-b2 = Book("The Catcher in the Rye")
+b1 = Book("War and Peace", "dummy", 50, 200)
+b2 = Book("The Catcher in the Rye", "dummy2", 51, 300)
 
 # TODO: print the price of book1
+print(b1.getprice())
 
+b1.setdiscount(0.25)
+print(b1.getprice())
+
+print(b1._Book__secret)
 
 # TODO: try setting the discount
-
 
 # TODO: properties with double underscores are hidden by the interpreter

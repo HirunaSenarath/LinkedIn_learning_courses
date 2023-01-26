7aac9cff2c15029037a833c115d137a129439f9e
Hiruna Senarath <49858479+HirunaSenarath@users.noreply.github.com>
2023-01-26 13:15:05 +0530

    Data classes default values code

--- Python_OOP/Exercise Files/Ch 4/datadefault_start.py
+++ Python_OOP/Exercise Files/Ch 4/datadefault_start.py
@@ -1,13 +1,23 @@
 # Python Object Oriented Programming by Joe Marini course example
 # implementing default values in data classes
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+import random
+
+
+def page_func():
+    return random.randrange(10, 1000)
 
 
 @dataclass
 class Book:
     # you can define default values when attributes are declared
-    title: str
-    author: str
-    pages: int
-    price: float
+    title: str = "No title"
+    author: str = "No author"
+    pages: int = field(default_factory = page_func)
+    price: float = field(default = 0.0)
+
+
+b1 = Book("War and Peace", "Leo Tolstoy")
+b2 = Book("The Catcher in the Rye", "JD Salinger", 234)
+print(b1)
